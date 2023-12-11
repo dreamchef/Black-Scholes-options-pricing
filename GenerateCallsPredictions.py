8040f536f6f3b141113082976bc144d415e258e2
Dani Lisle <121541458+slowHands7@users.noreply.github.com>
2023-12-10 22:24:29 -0700

    filter out NaaN predictions resulting from extremely high volatility values

--- GenerateCallsPredictions.py
+++ GenerateCallsPredictions.py
@@ -1,6 +1,7 @@
 import numpy as np
 import scipy.stats as si
 import pandas as pd
+from sklearn.metrics import r2_score
 
 def black_scholes_call(S, X, T, r, sigma):
     """
@@ -13,7 +14,7 @@ def black_scholes_call(S, X, T, r, sigma):
     r (float): Risk-free interest rate
     sigma (float): Volatility of the stock's returns
     """
-    if sigma == 0 or T == 0:
+    if sigma == 0 or T == 0:  # Handling cases where volatility or time to maturity is zero
         return 0
 
     d1 = (np.log(S / X) + (r + 0.5 * sigma ** 2) * T) / (sigma * np.sqrt(T))
@@ -21,15 +22,20 @@ def black_scholes_call(S, X, T, r, sigma):
     call_price = (S * si.norm.cdf(d1, 0.0, 1.0) - X * np.exp(-r * T) * si.norm.cdf(d2, 0.0, 1.0))
     return call_price
 
-# Get data from scraped file.
+# File path for the uploaded CSV
 file_path = '/mnt/data/calls_dataset.csv'
+
+# Loading the CSV file
 df_calls = pd.read_csv(file_path)
 
-# Set risk free rate.
-risk_free_rate = 0.423  # Example: 1%
+# Setting a constant risk-free rate for all options (4.23% in this case)
+risk_free_rate = 0.0423
 
-# Generate call prices.
-df_calls['Predicted Call Price'] = df_calls.apply(
+# Filtering out rows with extremely high volatility
+df_filtered = df_calls[df_calls['impliedVolatility'] <= 3]
+
+# Recalculating the call prices for the filtered data
+df_filtered['Predicted Call Price'] = df_filtered.apply(
     lambda row: black_scholes_call(
         S=row['price'],
         X=row['strike'],
@@ -40,9 +46,10 @@ df_calls['Predicted Call Price'] = df_calls.apply(
     axis=1
 )
 
-# Prepare dataframe.
-final_df = df_calls[['contractSymbol', 'bid', 'ask', 'Predicted Call Price']]
+# Further cleaning to remove any NaN or infinite values in the columns 'bid', 'ask', and 'Predicted Call Price'
+df_filtered_clean = df_filtered.dropna(subset=['bid', 'ask', 'Predicted Call Price'])
+df_filtered_clean = df_filtered_clean.replace([np.inf, -np.inf], np.nan).dropna(subset=['bid', 'ask', 'Predicted Call Price'])
 
-# Save dataframe to CSV file.
-output_csv_path = '/mnt/data/predicted_call_prices.csv'
-final_df.to_csv(output_csv_path, index=False)
+# Saving the cleaned and updated DataFrame to a new CSV file
+output_csv_path_new_predictions = '/mnt/data/predicted_call_prices_new.csv'
+df_filtered_clean.to_csv(output_csv_path_new_predictions, index=False)
