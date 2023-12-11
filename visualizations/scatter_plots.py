ca78549017e9381b47624508fa008e0929244dcd
Dani <121541458+slowHands7@users.noreply.github.com>
2023-12-11 12:57:30 -0700

    re-org

--- visualizations/scatter_plots.py
+++ visualizations/scatter_plots.py
@@ -2,9 +2,8 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
 
-df = pd.read_csv('predicted_call_prices.csv')
+df = pd.read_csv('result_data/predicted_call_prices.csv')
 
-# Scatter Plot for Predicted Call Price vs. Bid Price.
 plt.figure(figsize=(10, 6))
 sns.scatterplot(x=df['Predicted Call Price'], y=df['bid'])
 plt.title('Predicted Call Price vs. Bid Price')
@@ -14,7 +13,6 @@ plt.grid(True)
 plt.savefig('scatter_predicted_vs_bid.png')
 plt.show()
 
-# Scatter Plot for Predicted Call Price vs. Ask Price.
 plt.figure(figsize=(10, 6))
 sns.scatterplot(x=df['Predicted Call Price'], y=df['ask'])
 plt.title('Predicted Call Price vs. Ask Price')
