1e646c0ef469d309b5745b6874eb32e22bae465d
Dani Lisle <121541458+slowHands7@users.noreply.github.com>
2023-12-10 21:57:26 -0700

    Modify GenerateCallsPredictions.py

--- GenerateCallsPredictions.py
+++ GenerateCallsPredictions.py
@@ -13,27 +13,36 @@ def black_scholes_call(S, X, T, r, sigma):
     r (float): Risk-free interest rate
     sigma (float): Volatility of the stock's returns
     """
+    if sigma == 0 or T == 0:
+        return 0
+
     d1 = (np.log(S / X) + (r + 0.5 * sigma ** 2) * T) / (sigma * np.sqrt(T))
     d2 = d1 - sigma * np.sqrt(T)
     call_price = (S * si.norm.cdf(d1, 0.0, 1.0) - X * np.exp(-r * T) * si.norm.cdf(d2, 0.0, 1.0))
     return call_price
 
-def calculate_call_prices(input_csv, output_csv):
-    """
-    Read option data from a CSV file, calculate call prices, and write to another CSV file.
+# Get data from scraped file.
+file_path = '/mnt/data/calls_dataset.csv'
+df_calls = pd.read_csv(file_path)
 
-    Parameters:
-    input_csv (str): File path for the input CSV file
-    output_csv (str): File path for the output CSV file
-    """
-    # Read the data from CSV
-    df = pd.read_csv(input_csv)
+# Set risk free rate.
+risk_free_rate = 0.423  # Example: 1%
 
-    # Calculate call prices
-    df['Call Price'] = df.apply(lambda row: black_scholes_call(row['Stock Price'], row['Strike Price'], row['Time to Maturity'], row['Risk-free Rate'], row['Volatility']), axis=1)
+# Generate call prices.
+df_calls['Predicted Call Price'] = df_calls.apply(
+    lambda row: black_scholes_call(
+        S=row['price'],
+        X=row['strike'],
+        T=row['remaining'] / 365,  # converting days to years
+        r=risk_free_rate,
+        sigma=row['impliedVolatility']
+    ),
+    axis=1
+)
 
-    # Write the results to a new CSV file
-    df.to_csv(output_csv, index=False)
+# Prepare dataframe.
+final_df = df_calls[['contractSymbol', 'bid', 'ask', 'Predicted Call Price']]
 
-# Example usage
-calculate_call_prices('calls_dataset.csv', 'predicted_call_prices.csv')
+# Save dataframe to CSV file.
+output_csv_path = '/mnt/data/predicted_call_prices.csv'
+final_df.to_csv(output_csv_path, index=False)
